ac5eed8f1608ccd9ad77cfc0800dfdfb9e086c7c
victor <marslandervb@hotmail.com>
2020-04-02 21:12:25 +0200

    spaces are now proper tabs

--- hansibot/hansibot.py
+++ hansibot/hansibot.py
@@ -13,30 +13,30 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+	# we do not want the bot to reply to itself
+	if message.author == client.user:
+		return
 
-    # Everything in this if-statement is for universe-admins and universe-mods only:
-    if staff[0] in [y.id for y in message.author.roles] or staff[1] in [y.id for y in message.author.roles]:
-        if message.content.startswith('!foradminsonly'):
-            msg = '{0.author.mention} is an admin'.format(message)
-            await client.send_message(message.channel, msg)
+	# Everything in this if-statement is for universe-admins and universe-mods only:
+	if staff[0] in [y.id for y in message.author.roles] or staff[1] in [y.id for y in message.author.roles]:
+		if message.content.startswith('!foradminsonly'):
+			msg = '{0.author.mention} is an admin'.format(message)
+			await client.send_message(message.channel, msg)
 
-        if message.content.startswith('!whitelist add'):
-            content = message.content.split()
-            msg = 'Whitelisting {}'.format(content[2], message)
-            await client.send_message(message.channel, msg)
-            whitelistingOutput = subprocess.check_output([whitelistScript, content[2]], stderr=subprocess.STDOUT).decode('utf-8')
-            msg = 'Output terminal: {}'.format(whitelistingOutput, message)
-            await client.send_message(message.channel, msg)
+		if message.content.startswith('!whitelist add'):
+			content = message.content.split()
+			msg = 'Whitelisting {}'.format(content[2], message)
+			await client.send_message(message.channel, msg)
+			whitelistingOutput = subprocess.check_output([whitelistScript, content[2]], stderr=subprocess.STDOUT).decode('utf-8')
+			msg = 'Output terminal: {}'.format(whitelistingOutput, message)
+			await client.send_message(message.channel, msg)
 
-        if message.content.startswith('!ip'):
-            ipOutput = subprocess.check_output([curl, ipsite], stderr=subprocess.STDOUT).decode('utf-8')
-            msg = 'Output terminal: {}'.format(ipOutput, message)
-            await client.send_message(message.channel, msg)
-        
-        if "!HeilHansi" in message.content:
+		if message.content.startswith('!ip'):
+			ipOutput = subprocess.check_output([curl, ipsite], stderr=subprocess.STDOUT).decode('utf-8')
+			msg = 'Output terminal: {}'.format(ipOutput, message)
+			await client.send_message(message.channel, msg)
+		
+		if "!HeilHansi" in message.content:
 			msg = 'Danke schön {0.author.mention}! https://wetfjord.eu/heilhansi.png '.format(message)
 			await client.send_message(message.channel, msg)
 
@@ -47,9 +47,9 @@ async def on_message(message):
 			await client.send_message(message.channel, msg)
 			
 ###### non-admin commands: ######
-    if "hansi sucks" in message.content:
-        msg = 'You said fucking wut m8?! Fight me bitch'.format(message)
-        await client.send_message(message.channel, msg)
+	if "hansi sucks" in message.content:
+		msg = 'You said fucking wut m8?! Fight me bitch'.format(message)
+		await client.send_message(message.channel, msg)
 
 	if "!commanderhansi" in message.content:
 		commander_list = ['Yes comrade?', 'Well no shit. What\'ve we\'ve got here? A fucking comedian, Private Joker.', 'knock knock who\'s there? COMMANDER HANSI!', 'I bet you\'re the kind of guy that would f-ck a person in the ass and not even have the goddamn common courtesy to give him a reach-around. I\'ll be watching you {}'.format(message.author.mention)]
@@ -59,10 +59,10 @@ async def on_message(message):
 
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
 
 
 client.run(TOKEN)

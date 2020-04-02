595645e2dc4345a1f095e2ce5ef6412eba267834
victor <marslandervb@hotmail.com>
2020-04-02 21:05:28 +0200

    Added !commanderhansi command. !Heilhansi, !Comradehansi are now for staff only

--- hansibot/hansibot.py
+++ hansibot/hansibot.py
@@ -35,20 +35,26 @@ async def on_message(message):
             ipOutput = subprocess.check_output([curl, ipsite], stderr=subprocess.STDOUT).decode('utf-8')
             msg = 'Output terminal: {}'.format(ipOutput, message)
             await client.send_message(message.channel, msg)
+        
+        if "!HeilHansi" in message.content:
+			msg = 'Danke schön {0.author.mention}! https://wetfjord.eu/heilhansi.png '.format(message)
+			await client.send_message(message.channel, msg)
 
+		if "!ComradeHansi" in message.content:
+			comrade_list = ['Yes comrade?', 'Heil H... Stalin!', 'knock knock who\'s there? COMRADE HANSI!', 'cyka blyat {}'.format(message.author.mention)]
+			comrade_item = random.choice(comrade_list)
+			msg = '{} https://wetfjord.eu/comradehansi.png '.format(comrade_item, message)
+			await client.send_message(message.channel, msg)
+			
+###### non-admin commands: ######
     if "hansi sucks" in message.content:
         msg = 'You said fucking wut m8?! Fight me bitch'.format(message)
         await client.send_message(message.channel, msg)
 
-    if "!HeilHansi" in message.content:
-        msg = 'Danke schön {0.author.mention}! https://wetfjord.eu/heilhansi.png '.format(message)
-        await client.send_message(message.channel, msg)
-
-    if "!ComradeHansi" in message.content:
-        comrade_list = ['Yes comrade?', 'Heil H... Stalin!', 'knock knock who\'s there? COMRADE HANSI!', 'cyka blyat {}'.format(message.author.mention)]
-        comrade_item = random.choice(comrade_list)
-        msg = '{} https://wetfjord.eu/comradehansi.png '.format(comrade_item, message)
-        await client.send_message(message.channel, msg)
+	if "!commanderhansi" in message.content:
+		commander_list = ['Yes comrade?', 'Well no shit. What\'ve we\'ve got here? A fucking comedian, Private Joker.', 'knock knock who\'s there? COMMANDER HANSI!', 'I bet you\'re the kind of guy that would f-ck a person in the ass and not even have the goddamn common courtesy to give him a reach-around. I\'ll be watching you {}'.format(message.author.mention)]
+		commander_item = random.choice(commander_list)
+		msg = ' https://wetfjord.eu/commanderhansi.png {}'.format(commander_item, message)
 
 
 @client.event

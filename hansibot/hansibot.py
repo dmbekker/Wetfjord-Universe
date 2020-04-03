2577d58e2f450602f884bcc5374d55eefec03f5e
Victor <marslandervb@hotmail.com>
2020-04-03 20:40:47 +0200

    Playercount is 2 rows down now

--- hansibot/hansibot.py
+++ hansibot/hansibot.py
@@ -96,7 +96,7 @@ async def on_message(message):
             for player in players:
                 playtime_minutes = player.duration / 60
                 players_and_score += player.name + "\t \t" + str(player.score) + "\t \t" + str(playtime_minutes) + "\n"
-            msg = '``` Players online: {} \n Map: {} \n Game: {} \n Name \t \t Score \t \t Playtime (minutes) \n {} ```'.format(player_count, game_info.map_name, game_info.game, players_and_score)
+            msg = '``` Map: {} \n Game: {} \n Players online: {} \n Name \t \t Score \t \t Playtime (minutes) \n {} ```'.format( game_info.map_name, game_info.game, player_count, players_and_score)
             await client.send_message(message.channel, msg)
 
 @client.event

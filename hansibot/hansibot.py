c2275eeb5a240cc53d61b3aeede389dbe5548094
Victor <marslandervb@hotmail.com>
2020-06-05 20:51:41 +0200

    Adding support to show players in playerstatus

--- hansibot/hansibot.py
+++ hansibot/hansibot.py
@@ -6,7 +6,6 @@ import discord
 import subprocess
 import random
 import a2s
-from discord.ext import commands
 import asyncio
 from hansibotConfig import *
 
@@ -21,8 +20,31 @@ client = discord.Client()
 
 async def update_now_playing():
     await client.wait_until_ready()
-    #while True:
-    while not client.is_closed:
+    while not client.is_closed():
+        # hl2dm
+        players_hl2 = (a2s.players(address_hl2dm))
+        player_count_hl2 = len(players_hl2)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
+                                                               name="{} players in HL2:DM".format(
+                                                                   player_count_hl2)))
+        await asyncio.sleep(10)
+
+        # tf2
+        players_tf2 = (a2s.players(address_tf2))
+        player_count_tf2 = len(players_tf2)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
+                                                               name="{} players in TF2".format(player_count_tf2)))
+        await asyncio.sleep(10)
+
+        # Arma3
+        players_arma3 = (a2s.players(address_arma3))
+        player_count_arma3 = len(players_arma3)
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
+                                                               name="{} players in Arma 3".format(
+                                                                   player_count_arma3)))
+        await asyncio.sleep(10)
+
+    while not client.is_closed():
         #hl2dm
         players_hl2 = (a2s.players(address_hl2dm))
         player_count_hl2 = len(players_hl2)
@@ -140,4 +162,3 @@ async def on_ready():
 
 client.loop.create_task(update_now_playing())
 client.run(TOKEN)
-
